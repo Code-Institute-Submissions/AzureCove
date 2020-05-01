0bea7228d1c5c8cfb2a04f1716bbb705d92e547c
misza80 <55979600+misza80@users.noreply.github.com>
2020-05-01 14:20:48 +0100

    added navigation script for SPA

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-/*Navbar colour change script*/
+//Navbar colour change script
+
 $(window).on("scroll", function () {
   if ($(window).scrollTop() > 640) {
     $(".navbar").addClass("active");
@@ -6,16 +7,18 @@ $(window).on("scroll", function () {
     $(".navbar").removeClass("active");
   }
 });
-/*Typing script*/
+
+//Typing script
 $(document).ready(function () {
   var typed = new Typed(".typing", {
-    strings: ["housewives,", "students,", "junior developers,"],
+    //Words that are being typed by the script.
+    strings: ["students,", "junior developers,"],
     typeSpeed: 50,
     backSpeed: 50,
     loop: true, // Default value
   });
 });
-/*Wave script*/
+//Wave script
 $(document).ready(function () {
   loop();
   function loop() {
@@ -25,7 +28,8 @@ $(document).ready(function () {
     });
   }
 });
-/*subscribe modal script*/
+
+//subscribe modal
 document.getElementById("buttonmod").addEventListener("click", function () {
   document.querySelector(".bg-modal").style.display = "flex";
 });
@@ -33,3 +37,67 @@ document.getElementById("buttonmod").addEventListener("click", function () {
 document.querySelector(".close").addEventListener("click", function () {
   document.querySelector(".bg-modal").style.display = "none";
 });
+
+//navigation for the single page app.
+// It loads html code from external html files.
+// External HTML pages after visited once are then stored.
+// Keys correspond to piece identifiers.
+// Values are the text content of each loaded external HTML file.
+var partialHtmlCache = {};
+
+// HTTP GET request using XMLHttpRequest.
+// Fetches the file at the given path, then
+// calls the callback with the text content of the file.
+function fetchFile(path, callback) {
+  // Create a new AJAX request for fetching the partial HTML file.
+  var request = new XMLHttpRequest();
+
+  request.onload = function () {
+    callback(request.responseText);
+  };
+  request.open("GET", path);
+  request.send(null);
+}
+
+// This function implements a simple cache.
+function getContent(pieceId, callback) {
+  // If the page has been fetched before,
+  if (partialHtmlCache[pieceId]) {
+    // Pass the previously fetched content to the callback.
+    callback(partialHtmlCache[pieceId]);
+  } else {
+    // If the page has not been fetched before.
+    fetchFile(pieceId + ".html", function (content) {
+      // Store the fetched content in the cache.
+      partialHtmlCache[pieceId] = content;
+
+      // Pass the newly fetched content to the callback.
+      callback(content);
+    });
+  }
+}
+
+// Updates the content based on the piece identifier.
+function navigate() {
+  // Get a reference to the "contentcont" div.
+  var contentDiv = document.getElementById("contentcont"),
+    // Remove "#" character.
+    pieceId = location.hash.substr(1);
+
+  // Change the "contentcont" div innerHTML based on the piece identifier.
+  getContent(pieceId, function (content) {
+    contentDiv.innerHTML = content;
+  });
+}
+
+// If no piece identifier is provided,
+if (!location.hash) {
+  // default to #home.
+  location.hash = "#home";
+}
+
+// Navigate once to the initial piece identifier.
+navigate();
+
+// Navigate whenever the piece identifier value changes.
+window.addEventListener("hashchange", navigate);
